c861b4f300f3d7c0bd344fbe62db89f66bfc4f11
Jose Garcia <josegarcia19809@gmail.com>
2025-01-12 22:55:39 -0600

    Spring data JPA: Configurando datasource y Postgres con docker

--- src/main/java/com/example/best_travel/BestTravelApplication.java
+++ src/main/java/com/example/best_travel/BestTravelApplication.java
@@ -1,5 +1,6 @@
 package com.example.best_travel;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BestTravelApplication {
 
 	public static void main(String[] args) {
+		//Dotenv dotenv = Dotenv.configure().load();
+		// Cargar archivo .env manualmente
+		Dotenv dotenv = Dotenv.configure().filename(".env").load();
+
+		// Opcional: Imprime para verificar si las variables están cargadas
+		System.out.println("DB_USERNAME: " + dotenv.get("DB_USERNAME"));
+		System.out.println("DB_PASSWORD: " + dotenv.get("DB_PASSWORD"));
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(BestTravelApplication.class, args);
 		System.out.println("Best travel application is running");
 	}
